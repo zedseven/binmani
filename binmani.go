a2d3682acd53531d6ecb1d8ef8107ac6a57f2028
Zacchary Dempsey-Plante <zacchary.dempseyplante@gmail.com>
2020-02-02 15:52:49 -0500

    Added functions for converting to and from byte slices.

--- binmani.go
+++ binmani.go
@@ -1,6 +1,8 @@
 // Package binmani provides rudimentary binary manipulation functions.
 package binmani
 
+const bitsPerByte int = 8
+
 // Bit manipulation functions
 
 // GetMask creates a bitmask of size shifted left index bits.
@@ -10,11 +12,44 @@ func GetMask(index, size uint8) uint16 {
 }
 
 // ReadFrom reads a specified bit or set of consecutive bits from data.
+// index works from the right of the data to the left.
 func ReadFrom(data uint16, index, size uint8) uint16 {
 	return (data & GetMask(index, size)) >> index
 }
 
 // WriteTo writes a value to a specified bit or set of consecutive bits in data, and returns the result.
+// index works from the right of the data to the left.
 func WriteTo(data uint16, index, size uint8, value uint16) uint16 {
 	return (data & (^GetMask(index, size))) | (value << index)
+}
+
+// BytesToBits converts a byte slice to a slice of each individual bit of the bytes.
+func BytesToBits(bytes []byte) (bits []uint8) {
+	bits = make([]uint8, len(bytes) * bitsPerByte)
+	for i := 0; i < len(bytes); i++ {
+		for j := 0; j < bitsPerByte; j++ {
+			bits[i * bitsPerByte + j] = uint8(ReadFrom(uint16(bytes[i]), uint8(bitsPerByte - j - 1), 1))
+		}
+	}
+	return
+}
+
+// BitsToBytes converts a slice of individual bits into a slice of bytes, effectively compressing them together.
+func BitsToBytes(bits []uint8) (bytes []byte) {
+	numBytes := len(bits) / bitsPerByte
+	if len(bits) % bitsPerByte != 0 {
+		numBytes++
+	}
+	bytes = make([]byte, numBytes)
+	for i := 0; i < numBytes; i++ {
+		for j := 0; j < bitsPerByte; j++ {
+			//bits[i * bitsPerByte + j] = uint8(ReadFrom(uint16(bytes[i]), uint8(bitsPerByte - j - 1), 1))
+			// Zero-pad the beginning of the array if the number of bits is not a multiple of 8
+			if i == 0 && j < len(bits) % bitsPerByte {
+				continue
+			}
+			bytes[i] = byte(WriteTo(uint16(bytes[i]), uint8(bitsPerByte - j - 1), 1, uint16(bits[i * bitsPerByte + j])))
+		}
+	}
+	return
 }
